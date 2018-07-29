fd23e091b1023783c23e81dd50f000d65b239518
karayuumac <karaatena0706@gmail.com>
2018-07-29 13:21:21 +0900

    update spongepowered version

--- src/main/kotlin/InfinityCircles.kt
+++ src/main/kotlin/InfinityCircles.kt
@@ -1,9 +1,13 @@
 import com.google.inject.Inject
+import ninja.leaping.configurate.commented.CommentedConfigurationNode
+import ninja.leaping.configurate.loader.ConfigurationLoader
 import org.slf4j.Logger
+import org.spongepowered.api.config.DefaultConfig
 import org.spongepowered.api.event.Listener
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent
 import org.spongepowered.api.event.game.state.GameStoppingEvent
 import org.spongepowered.api.plugin.Plugin
+import java.io.File
 
 /**
  * Created by karayuu on 2018/06/26
@@ -13,7 +17,17 @@ import org.spongepowered.api.plugin.Plugin
 class InfinityCircles {
     @Inject lateinit var logger: Logger
         private set
+    /*
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    lateinit var configFile: File
+        private set
 
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    lateinit var configLoader: ConfigurationLoader<CommentedConfigurationNode>
+        private set
+    */
     @Listener
     fun onPreInitialization(event: GamePreInitializationEvent) {
         logger.info("Start plugin.")
